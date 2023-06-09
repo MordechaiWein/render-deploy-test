7b2f4390771d59f97a907a7aa056f79200d3deda
Mordechai <mordwein77@gmail.com>
2023-06-09 17:41:41 -0400

    seed data 5

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,17 +1,17 @@
-User.destroy_all
+# User.destroy_all
 
-puts "seeding data..."
+# puts "seeding data..."
 
-User.create(name: 'Max', age: 10)
-User.create(name: 'James', age: 20)
-User.create(name: 'William', age: 35)
-User.create(name: 'John', age: 53)
-User.create(name: 'Stanley', age: 12)
-User.create(name: 'Rebecca', age: 17)
-User.create(name: 'Tiffany', age: 22)
-User.create(name: 'Wendy', age: 44)
-User.create(name: 'Susan', age: 16)
-User.create(name: 'Katie', age: 29)
+# User.create(name: 'Max', age: 10)
+# User.create(name: 'James', age: 20)
+# User.create(name: 'William', age: 35)
+# User.create(name: 'John', age: 53)
+# User.create(name: 'Stanley', age: 12)
+# User.create(name: 'Rebecca', age: 17)
+# User.create(name: 'Tiffany', age: 22)
+# User.create(name: 'Wendy', age: 44)
+# User.create(name: 'Susan', age: 16)
+# User.create(name: 'Katie', age: 29)
 
 
-puts "seeding complete 🌱"
+# puts "seeding complete 🌱"
